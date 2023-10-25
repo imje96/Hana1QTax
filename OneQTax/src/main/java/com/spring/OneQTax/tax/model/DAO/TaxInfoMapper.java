bde254721afcc0c0f5357b765bb8b439f0289075
imje96 <127702320+imje96@users.noreply.github.com>
2023-10-25 23:59:06 +0900

    .

--- OneQTax/src/main/java/com/spring/OneQTax/tax/model/DAO/TaxInfoMapper.java
+++
@@ -1,9 +0,0 @@
-package com.spring.OneQTax.tax.model.DAO;
-
-import com.spring.OneQTax.tax.model.DTO.TaxInfoDTO;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface TaxInfoMapper {
-    TaxInfoDTO getTaxInfoById(int id);
-}
